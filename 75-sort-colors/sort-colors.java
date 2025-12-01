e5ab092888fc50684dcf743805810afc99b185ed
Vishal M <vishalm1415@gmail.com>
2025-12-01 18:40:23 +0530

    Time: 2 ms (14.67%) | Memory: 43.6 MB (15.04%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        for(int i =0;i<n;i++){
+            for(int j =1;j<n-i;j++){
+                if(nums[j] < nums[j-1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j-1];
+                    nums[j-1] = temp;
+                }
+            }
+        }
+    }
+}
