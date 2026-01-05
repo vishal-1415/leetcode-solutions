6473de71e90604791dffb34cd836a32c0945e1ad
Vishal M <vishalm1415@gmail.com>
2026-01-05 22:43:16 +0530

    Time: 3 ms (90.00%) | Memory: 44.1 MB (92.14%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
@@ -13,3 +13,17 @@ class Solution {
         return max;
     }
 }
+
+// class Solution {
+//     public int mostWordsFound(String[] sentences) {
+//         int n = sentences.length;
+//         int Maximum = 0;
+//         for(int i =0;i<n;i++){
+//             int current_max = sentences[i].split(" ").length;
+//             if(current_max > Maximum){
+//                 Maximum = current_max;
+//             }
+//         }
+//         return Maximum;
+//     }
+// }
