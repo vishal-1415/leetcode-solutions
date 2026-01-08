745e1386b2e009df334a14bf6ec973aef30f851b
Vishal M <vishalm1415@gmail.com>
2026-01-08 23:30:23 +0530

    Time: 6 ms (77.34%) | Memory: 67.2 MB (66.17%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,20 +1,41 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
 
-        // add only unquie number to unqiueNums using set
-        Set<Integer> unqiueNums = new HashSet<>();
-        for (int num : nums) {
-            unqiueNums.add(num);
+   //If a number is missing, its index never gets marked negative — so it stays positive.
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
         }
-
-
-        List<Integer> resultList = new ArrayList<>();
-        for (int i = 1; i <= nums.length; i++) {             // Complexity
-            if (!unqiueNums.contains(i)) {                  //    Time: O(n)
-                resultList.add(i);                           //   Space: O(n)
+                                                                    //Time: O(n)
+        List<Integer> resultList = new ArrayList<>();               //Space: O(1) (excluding output list)
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                resultList.add(i+1);
             }
         }
 
         return resultList;
     }
 }
+
+// class Solution {
+//     public List<Integer> findDisappearedNumbers(int[] nums) {
+
+//         // add only unquie number to unqiueNums using set
+//         Set<Integer> unqiueNums = new HashSet<>();
+//         for (int num : nums) {
+//             unqiueNums.add(num);
+//         }
+
+//         List<Integer> resultList = new ArrayList<>();
+//         for (int i = 1; i <= nums.length; i++) {             // Complexity
+//             if (!unqiueNums.contains(i)) {                  //    Time: O(n)
+//                 resultList.add(i);                           //   Space: O(n)
+//             }
+//         }
+
+//         return resultList;
+//     }
+// }
