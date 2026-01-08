1e47cf7ed0225c1fc91b14c9c81e7fb3c1f620e1
Vishal M <vishalm1415@gmail.com>
2026-01-08 22:34:55 +0530

    Time: 19 ms (34.06%) | Memory: 74.3 MB (23.36%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,17 +1,20 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
 
-        Set<Integer> UniqueNums = new HashSet<>();
+        // add only unquie number to unqiueNums using set
+        Set<Integer> unqiueNums = new HashSet<>();
         for (int num : nums) {
-            UniqueNums.add(num);
+            unqiueNums.add(num);
         }
-        List<Integer> result = new ArrayList<>();
-        for (int i = 1; i <=nums.length; i++) {
-            if (!UniqueNums.contains(i)) {
-                result.add(i);
+
+
+        List<Integer> resultList = new ArrayList<>();
+        for (int i = 1; i <= nums.length; i++) {             // Complexity
+            if (!unqiueNums.contains(i)) {                  //    Time: O(n)
+                resultList.add(i);                           //   Space: O(n)
             }
         }
 
-        return result;
+        return resultList;
     }
 }
