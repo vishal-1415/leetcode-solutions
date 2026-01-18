3e85399b7fe558f81316b8468ae9ee631dbd6a0c
Vishal M <vishalm1415@gmail.com>
2026-01-18 12:20:49 +0530

    Time: 3 ms (13.60%) | Memory: 43.5 MB (67.28%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,12 +1,12 @@
 class Solution {
     public void sortColors(int[] nums) {
         int n = nums.length;
-        for(int i = 0;i<n;i++){
-            for(int j = 1;j<n-i;j++){
-                if(nums[j] < nums[j-1]){
-                    int temp = nums[j-1];
-                    nums[j-1] = nums[j];
-                    nums[j]  = temp;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n - i; j++) {
+                if (nums[j] < nums[j - 1]) {
+                    int temp = nums[j - 1];         //This is optimized Bubble Sort
+                    nums[j - 1] = nums[j];          // TC :O(n^2)  SC : O(1)
+                    nums[j] = temp;
                 }
             }
         }
