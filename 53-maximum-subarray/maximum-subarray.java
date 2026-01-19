4e8744776117c17a955a86004f36ef8f2cf3b1f9
Vishal M <vishalm1415@gmail.com>
2026-01-19 22:35:04 +0530

    Time: 1 ms (99.63%) | Memory: 77.5 MB (16.89%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -4,8 +4,8 @@ class Solution {
         int max = nums[0];
         int current = nums[0];
         for (int i = 1; i < n; i++) {
-            current = Math.max(nums[i], current + nums[i]);
-            max = Math.max(current, max);
+            current = Math.max(nums[i], current + nums[i]);     //Kadane’s algorithm 
+            max = Math.max(current, max);                       //O(n), O(1)
         }
         return max;
     }
