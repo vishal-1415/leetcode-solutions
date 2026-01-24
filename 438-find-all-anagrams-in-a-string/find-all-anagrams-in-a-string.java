ad535460fe60f2a6610f296c59907996ebe20658
Vishal M <vishalm1415@gmail.com>
2026-01-24 23:59:42 +0530

    Time: 13 ms (49.93%) | Memory: 47.1 MB (39.59%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,23 +1,25 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
+
         int[] sfreqMap = new int[26];
         int[] pfreqMap = new int[26];
         List<Integer> result = new ArrayList<>();
-        for(char freq : p.toCharArray()){                          
+        // creating or count freq of p ,"abc" --> [1,1,1,0,0...]
+        for (char freq : p.toCharArray()) {
             pfreqMap[freq - 'a']++;
         }
-        for(int i =0 ;i<s.length();i++){
+        //Sliding window to count freq of s
+        for (int i = 0; i < s.length(); i++) {
             sfreqMap[s.charAt(i) - 'a']++;
 
-            if(i>=p.length()){
+            if (i >= p.length()) {  //removing the char that is out of window
                 sfreqMap[s.charAt(i - p.length()) - 'a']--;
             }
-
-            if(Arrays.equals(sfreqMap,pfreqMap)){
-                result.add(i - p.length() +1);
+            // checks counts of freqs of both sfreqMap and pfreqMap 
+            if (Arrays.equals(sfreqMap, pfreqMap)) {
+                result.add(i - p.length() + 1);
             }
-        }
-
+        }                                //TC:O(n)  SC:O(1)
         return result;
     }
 }
