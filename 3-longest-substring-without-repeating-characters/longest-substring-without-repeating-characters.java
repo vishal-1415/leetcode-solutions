fdb745150d8a9ea59c353bce4ea5ea0926976358
Vishal M <vishalm1415@gmail.com>
2026-01-28 22:42:30 +0530

    Time: 6 ms (68.06%) | Memory: 46.7 MB (25.20%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -4,15 +4,19 @@ class Solution {
         int MaxLength = 0;
         int left = 0;
 
-        for(int right =0;right< s.length();right++){
+        for (int right = 0; right < s.length(); right++) {
 
-            while(CharSet.contains(s.charAt(right))){
+            // If current character already exists in set,
+            // shrink window from the left until duplicate is removed
+            while (CharSet.contains(s.charAt(right))) {
                 CharSet.remove(s.charAt(left));
-                left++;
+                left++;                                            //TC:O(n)   SC:O(min(n, charset))
             }
-            int currentLength = right - left +1;
-            CharSet.add(s.charAt(right));
-            MaxLength = Math.max(MaxLength , currentLength);
+
+            int currentLength = right - left + 1; //window Size
+
+            CharSet.add(s.charAt(right));// if not Add current character to the set
+            MaxLength = Math.max(MaxLength, currentLength);
         }
         return MaxLength;
     }
