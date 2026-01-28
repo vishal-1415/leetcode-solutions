7bc9994f6dd0f516b31bb0b8833d7eefc9bbad20
Vishal M <vishalm1415@gmail.com>
2026-01-28 23:30:25 +0530

    Time: 12 ms (48.58%) | Memory: 46.1 MB (86.89%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,24 @@
+ class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] FreqMap = new int[26];
+        int left = 0;
+        int maxfreq = 0;
+        int MaxLength = 0;
+
+        for(int right=0;right<s.length();right++){
+
+            FreqMap[s.charAt(right) - 'A']++; //update current freq of char
+            maxfreq = Math.max(maxfreq,FreqMap[s.charAt(right) - 'A']); 
+
+            int windowlength = right - left+1;
+
+            if(windowlength - maxfreq >k){   //if windowlength - maxfreq >k ,we need shrink win
+                FreqMap[s.charAt(left) - 'A']--;
+                left++;
+            }
+            windowlength = right - left+1;
+            MaxLength = Math.max(windowlength,MaxLength);
+        }
+        return MaxLength;
+    }
+}
