04be2301b765a9640c157e808311964b7eba7c23
Vishal M <vishalm1415@gmail.com>
2026-02-02 01:16:44 +0530

    Time: 272 ms (5.92%) | Memory: 47.4 MB (26.94%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -2,12 +2,14 @@ class Solution {
     public int maxProduct(int[] nums) {
         int n = nums.length;
         int max_product = nums[0];
-
+                                           //TC:O(n^2)   SC:O(1)
         for (int i = 0; i < n; i++) {
-            int current_product = 1;
+            int current_product = 1; // initized to 1 bcuz, 0 * anything is Zero
             for (int j = i; j < n; j++) {
+
                 current_product *= nums[j];
                 max_product = Math.max(max_product, current_product);
+
             }
         }
         return max_product;
