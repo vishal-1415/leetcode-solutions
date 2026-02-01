86a45e8d8d78397b79a8d377f7702aaa05ba421e
Vishal M <vishalm1415@gmail.com>
2026-02-02 00:27:55 +0530

    Time: 1 ms (98.17%) | Memory: 47.7 MB (17.53%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,16 +1,19 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int n = nums.length;
+        int n = nums.length;                                       //TC:O(n)  SC:O(1)
         int rightSum = 0;
-        for(int i =0;i<n;i++){
+        /// summing all nums into rightSum (prefixSum)
+        for (int i = 0; i < n; i++) {
             rightSum += nums[i];
-        }
+        }     
         int leftSum = 0;
-        for(int i =0;i<n;i++){
+        //adding leftSum elements one by one,keeps checking rightSum == leftSum
+        for (int i = 0; i < n; i++) {
             leftSum += nums[i];
-            if(rightSum == leftSum){
+            if (rightSum == leftSum) {
                 return i;
             }
+            //after everycheck keeps reducing rightSum windows Sum ,meanwhile incre leftSum 
             rightSum -= nums[i];
         }
         return -1;
