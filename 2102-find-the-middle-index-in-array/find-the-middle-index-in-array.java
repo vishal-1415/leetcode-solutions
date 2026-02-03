6873c135de4f20bcee23b960903a3d435934f551
Vishal M <vishalm1415@gmail.com>
2026-02-04 00:44:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (71.12%) - LeetSync

--- 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.java
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.java
@@ -2,13 +2,13 @@ class Solution {
     public int findMiddleIndex(int[] nums) {
         int n = nums.length;
         int leftsum = 0;
-        for(int i =0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             leftsum += nums[i];
         }
-        int rightsum = 0;
-        for(int i =0;i<n;i++){
-            rightsum += nums[i];
-            if(leftsum == rightsum){
+        int rightsum = 0;             //same as problem 724. Find Pivot Index.
+        for (int i = 0; i < n; i++) {
+            rightsum += nums[i];        //TC:O(n) SC:O(1)
+            if (leftsum == rightsum) {
                 return i;
             }
             leftsum -= nums[i];
