ec128a84110c9c491d35e68341130ca7bf1b1811
Vishal M <vishalm1415@gmail.com>
2026-02-03 23:35:31 +0530

    Time: 2 ms (41.79%) | Memory: 94.8 MB (11.01%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -2,15 +2,16 @@ class Solution {
     public int maxProfit(int[] prices) {
         int Buy_Price = prices[0];
         int profit = 0;
-        for(int i =1;i<prices.length;i++){
-
-            if(prices[i] < Buy_Price){
-                Buy_Price = prices[i];
+                                     //similar to Kadane's Algorithm
+        for (int i = 1; i < prices.length; i++) {
+            // check if next prices is less than Buy_Price
+            if (prices[i] < Buy_Price) {  
+                Buy_Price = prices[i]; //update the lower prices   
             }
-                int current_profit = prices[i] - Buy_Price;
-                profit = Math.max(profit,current_profit);
-            
-        }
+            int current_profit = prices[i] - Buy_Price;
+            profit = Math.max(profit, current_profit);     
+
+        }                     //TC:O(n)  SC:O(1)
         return profit;
     }
 }
