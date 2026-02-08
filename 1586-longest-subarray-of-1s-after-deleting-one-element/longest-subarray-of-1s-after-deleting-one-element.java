97074496879723294113df3f0959951d2fa25bf1
Vishal M <vishalm1415@gmail.com>
2026-02-08 19:59:39 +0530

    Time: 3 ms (86.29%) | Memory: 65.6 MB (24.86%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -4,19 +4,20 @@ class Solution {
         int left = 0;
         int count_Zeros = 0;
         int result = 0;
-        for(int right =0;right<n;right++){
-            if(nums[right] == 0){
+             // Expand window
+        for (int right = 0; right < n; right++) {
+            if (nums[right] == 0) {
                 count_Zeros++;
             }
-
-            while(count_Zeros>1){
-                if(nums[left] == 0){
+            // If more than one zero, shrink window
+            while (count_Zeros > 1) {
+                if (nums[left] == 0) {
                     count_Zeros--;
                 }
                 left++;
-            }
-         result  = Math.max(result , right - left);
-        } 
+            }// right - left represents length after deleting one element
+            result = Math.max(result, right - left);
+        }
         return result;
     }
 }
