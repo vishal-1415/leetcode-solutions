9c42ff782d15f126009471f191cc7b57b7122cd0
Vishal M <vishalm1415@gmail.com>
2026-03-01 18:48:24 +0530

    Time: 21 ms (33.32%) | Memory: 47.5 MB (96.30%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,30 +1,28 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int Min_speed = 1;
-        int Max_speed = 0;
-
-        for (int p : piles) {
-            Max_speed = Math.max(p, Max_speed);
+        int max_speed = 0;
+        int min_speed = 1;
+        for (int pile : piles) {// to Find max speed to eat bananas
+            max_speed = Math.max(max_speed, pile);
         }
+        //binary search to find optimal speed 
+        while (min_speed < max_speed) {
+            int mid = min_speed + (max_speed - min_speed) / 2;
 
-        while (Min_speed < Max_speed) {
-            int mid = Min_speed + (Max_speed - Min_speed) / 2;        //binary Search 
-
-            if (EatwithinTime(piles, h, mid)) {
-                Max_speed = mid;
+            if (CanEatWithinTime(piles, h, mid)) {
+                max_speed = mid;
             } else {
-                Min_speed = mid + 1;
+                min_speed = mid + 1;
             }
         }
-        return Min_speed;
+        return min_speed;
     }
-
-    private boolean EatwithinTime(int[] piles, int h, int speed) {
+    // function to check the speed is enough or not
+    public static boolean CanEatWithinTime(int[] piles, int h, int mid) {
         int hours = 0;
-        for (int p : piles) {
-            hours += (int) Math.ceil((double) p / speed);
+        for (int i = 0; i < piles.length; i++) {
+            hours += (int) Math.ceil((double) piles[i] / mid);
         }
         return hours <= h;
     }
-
 }
