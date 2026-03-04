6eadb63926f0180005371371a3c89753ce900508
Vishal M <vishalm1415@gmail.com>
2026-03-04 12:12:55 +0530

    Time: 3 ms (86.81%) | Memory: 52.2 MB (67.83%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,9 +1,9 @@
 class Solution {
     public int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
-            return 0;
+            return 0; // egde case
         }
-        int No_of_islands = 0;
+        int No_of_islands = 0;    // DFS - Depth first search
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1') {
@@ -16,10 +16,10 @@ class Solution {
     }
     public static void dfs(char[][] grid, int i, int j) {
         if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
-            return;
+            return;  //edge case for dfs 
         }
-        grid[i][j] = '0';
-
+        grid[i][j] = '0'; // mark as visited 
+    // Tryind all possible directions 
         dfs(grid, i + 1, j);
         dfs(grid, i - 1, j);
         dfs(grid, i, j + 1);
