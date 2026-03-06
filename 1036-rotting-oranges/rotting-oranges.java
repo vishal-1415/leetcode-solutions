2cf59b2acbfe34ccc85ca441d51961010a99c4bd
Vishal M <vishalm1415@gmail.com>
2026-03-06 23:51:52 +0530

    Time: 1 ms (99.83%) | Memory: 43.7 MB (98.96%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -7,8 +7,7 @@ class Solution {
                     dfs(grid, i, j, 2);
                 }
             }
-        }
-        // check remaining fresh oranges
+        }// check remaining fresh oranges
         int max_time = 2;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
@@ -20,7 +19,6 @@ class Solution {
         }
         return max_time - 2;
     }
-
     public void dfs(int[][] grid, int i, int j, int time) {
         if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length)
             return;
