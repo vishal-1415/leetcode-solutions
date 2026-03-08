c2411dc579edff359e82177ef4f0506181eec616
Vishal M <vishalm1415@gmail.com>
2026-03-08 17:47:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (15.88%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,7 +2,7 @@ class Solution {
     public int rob(int[] nums) {
         if (nums.length < 2) return nums[0];
 
-        int prevRob = 0;
+        int prevRob = 0;      //TC: O(n)   SC:O(1)
         int maxRob = 0;
 
         for (int i = 0; i < nums.length; i++) {
