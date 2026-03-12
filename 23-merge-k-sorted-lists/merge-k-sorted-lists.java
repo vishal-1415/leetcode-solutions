bb9f2388fc8dd27f8464e7cb058760f63abb5dfe
Vishal M <vishalm1415@gmail.com>
2026-03-13 00:01:16 +0530

    Time: 1 ms (99.89%) | Memory: 46.6 MB (90.33%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+    if(lists == null||lists.length == 0){
+        return null;
+    }
+    return mergeKListsHelper(lists,0,lists.length-1);
+    }
+    public ListNode mergeKListsHelper(ListNode[] list,int start,int end) {
+        if(start == end){
+            return list[start];
+        }
+        if(start+1 == end){
+            return mergeTwoLists(list[start] ,list[end]);
+        }
+
+        int mid = start + (end - start)/2;
+        ListNode left = mergeKListsHelper(list,start,mid);
+        ListNode right = mergeKListsHelper(list,mid+1,end);
+        return mergeTwoLists(left,right);
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                temp.next = list1;
+                list1 = list1.next;
+            }else{
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next; // Move forward in merged list
+        }
+        // Attach remaining nodes (only one list will have nodes)
+        if(list1 != null) temp.next = list1;        
+        if(list2 != null) temp.next = list2;
+        
+    return dummy.next;                        // TC: O(n + m)      
+    } 
+}
