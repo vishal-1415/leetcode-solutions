c827a757a873be8dc7b074a701443ab6ca24b6bb
Vishal M <vishalm1415@gmail.com>
2026-05-26 23:43:00 +0530

    Time: 2 ms (48.76%) | Memory: 43.9 MB (46.29%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -4,14 +4,15 @@ class Solution {
         for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
 
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            map.put(ch, map.getOrDefault(ch, 0) + 1); 
         }
+        // another sol is to use two boolean array ,then check both arrays
 
         int count = 0;
 
         for (char ch = 'a'; ch <= 'z'; ch++) {
-            char upper = Character.toUpperCase(ch);
-
+            char upper = Character.toUpperCase(ch);   //TC:O(n) SC:O(1) -> bcuz the max 26 char can exit two times(52)
+ 
             if (map.containsKey(ch) && map.containsKey(upper)) {
                 count++;
             }
