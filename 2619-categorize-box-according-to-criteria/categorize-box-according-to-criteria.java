db1472ac376400067930c8795889b4a05fab3c51
Vishal M <vishalm1415@gmail.com>
2026-05-26 22:36:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (64.10%) - LeetSync

--- 2619-categorize-box-according-to-criteria/categorize-box-according-to-criteria.java
+++ 2619-categorize-box-according-to-criteria/categorize-box-according-to-criteria.java
@@ -1,21 +1,24 @@
 class Solution {
     public String categorizeBox(int length, int width, int height, int mass) {
-        if(isBulky(length,width,height) && mass >= 100){
+
+        
+        if (isBulky(length, width, height) && mass >= 100) {
             return "Both";
-        }else if(isBulky(length,width,height)){
+        } else if (isBulky(length, width, height)) {
             return "Bulky";
-        }else if(mass >= 100){
+        } else if (mass >= 100) {
             return "Heavy";
-        }else{
+        } else {
             return "Neither";
         }
     }
-    private static boolean isBulky(int length, int width, int height){
-        long volume = (long)length*width*height;
 
-        if(length >= 10000|| width >= 10000 || height >= 10000 || volume >= 1000000000L){
+    private static boolean isBulky(int length, int width, int height) {
+        long volume = (long) length * width * height;
+
+        if (length >= 10000 || width >= 10000 || height >= 10000 || volume >= 1000000000L) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
