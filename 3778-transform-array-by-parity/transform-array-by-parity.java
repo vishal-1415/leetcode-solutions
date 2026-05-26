f5a64d08066d71047041b3b5e822b0dda36c7885
Vishal M <vishalm1415@gmail.com>
2026-05-26 20:03:38 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (83.37%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.java
+++ 3778-transform-array-by-parity/transform-array-by-parity.java
@@ -1,14 +1,19 @@
 class Solution {
     public int[] transformArray(int[] nums) {
+        int Even_count = 0;
         for(int i = 0;i<nums.length;i++){
             if(nums[i] % 2 ==0){
-                nums[i] = 0;
-            }else{
-                nums[i] = 1;
+                Even_count++;
             }
         }
 
-        Arrays.sort(nums);
+        for(int i = 0;i< Even_count;i++){
+            nums[i] = 0;
+        }
+
+        for(int i = Even_count;i<nums.length;i++){
+            nums[i] = 1;
+        }
 
     return nums;
     }
