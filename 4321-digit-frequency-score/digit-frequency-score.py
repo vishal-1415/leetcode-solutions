f7b95e3237c2e83e1a8e73feca76a7a2460a4623
Vishal M <vishalm1415@gmail.com>
2026-05-31 20:05:25 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (36.36%) - LeetSync

---
+++ 4321-digit-frequency-score/digit-frequency-score.py
@@ -0,0 +1,9 @@
+class Solution:
+    def digitFrequencyScore(self, n: int) -> int:
+        sum = 0
+        while n>0:
+            digit = n % 10
+            sum += digit
+            n //= 10
+
+        return sum
