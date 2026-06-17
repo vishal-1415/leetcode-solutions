1f12480a01849ea0bccef889d3492c917488645a
Vishal M <vishalm1415@gmail.com>
2026-06-17 23:06:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (82.75%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.java
@@ -1,23 +1,23 @@
 class Solution {
     public String interpret(String command) {
-    //     StringBuilder sb = new StringBuilder();
-    //     for(int i = 0;i<command.length();i++){
-    //         char ch = command.charAt(i);
-    //         if(ch == 'G'){
-    //             sb.append("G");
-    //         }
-    //         if(ch == '('){
-    //             if(command.charAt(i+1) == ')'){
-    //                 sb.append("o");
-    //                 i++;
-    //             }
-    //             else{
-    //                 sb.append("al");
-    //                 i += 3;
-    //             }
-    //         }
-    //     }
-    // return sb.toString();
-    return command.replace("()" ,"o").replace("(al)","al");
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0;i<command.length();i++){
+            char ch = command.charAt(i);
+            if(ch == 'G'){
+                sb.append("G");
+            }
+            if(ch == '('){
+                if(command.charAt(i+1) == ')'){
+                    sb.append("o");
+                    i++;
+                }
+                else{
+                    sb.append("al");
+                    i += 3;
+                }
+            }
+        }
+    return sb.toString();
+    //return command.replace("()" ,"o").replace("(al)","al");
     }
 }
