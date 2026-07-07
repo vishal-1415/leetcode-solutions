41e42ed46cbc4128e92bf586e5c259fa1ca838c6
Vishal M <vishalm1415@gmail.com>
2026-07-07 21:50:06 +0530

    Time: 6 ms (8.37%) | Memory: 42.7 MB (52.17%) - LeetSync

---
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -0,0 +1,35 @@
+class Solution {
+    public long sumAndMultiply(int n) {
+        int sum = 0;
+        int x = 0;
+        int og = n;
+        while(n>0){
+            int digit = n % 10;
+            if(digit != 0){
+            x = x*10 + digit;
+            }
+            n /= 10;
+        }
+        
+        n = x;
+        int num =0;
+        while(n>0){
+            int digit = n % 10;
+            if(digit != 0){
+            num = num*10 + digit;
+            }
+            n /= 10;
+        }
+
+        int x_copy = num;
+        while(x_copy > 0){
+            int digit = x_copy % 10;
+            sum += digit;
+            x_copy /= 10;
+        }
+    System.out.println(sum);
+        long ans = (long)num * sum;
+
+    return ans;
+    }
+}
