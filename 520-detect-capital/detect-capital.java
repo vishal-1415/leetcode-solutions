cada4ddd5cd4319c70c299cf0a720bea03fa78ea
Vishal M <vishalm1415@gmail.com>
2026-07-21 21:54:57 +0530

    Time: 1 ms (89.47%) | Memory: 43 MB (90.63%) - LeetSync

---
+++ 520-detect-capital/detect-capital.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int capital  = 0;
+        for(char ch : word.toCharArray()){
+            if(Character.isUpperCase(ch)){
+                capital++;
+            }
+        }
+
+        if(capital == word.length() || capital == 0){
+            return true;
+        }
+        if(capital == 1 && Character.isUpperCase(word.charAt(0))){
+            return true;
+        }
+    return false;
+    }
+}
