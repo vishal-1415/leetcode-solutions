662631f7902b0b79087e2e5a425d2bc6b439a529
Vishal M <vishalm1415@gmail.com>
2026-08-24 14:26:58 +0530

    Time: 815 ms (30.72%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select s.user_id , ROUND(COALESCE(avg(c.action = 'confirmed'),0),2) as confirmation_rate 
+from Signups s
+left join Confirmations c on
+ s.user_id = c.user_id
+group by s.user_id 
+order by s.user_id
